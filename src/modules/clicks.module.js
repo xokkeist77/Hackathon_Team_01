cc6295803c02cb5112db6a9473696704c70c51fb
ururu42 <utepusechka@gmail.com>
2024-11-30 12:43:16 +0100

    Реализовала модуль Аналитика кликов

--- src/modules/clicks.module.js
+++ src/modules/clicks.module.js
@@ -1,5 +1,19 @@
-import {Module} from '../core/module'
+import { Module } from "../core/module";
 
 export class ClicksModule extends Module {
+  trigger() {
+    let count = 0;
+    console.log("triggered");
 
+    const addOne = () => {
+      count++;
+    };
+
+    document.addEventListener("click", addOne);
+
+    setTimeout(() => {
+      document.removeEventListener("click", addOne);
+      alert(`За 3 секунды ты сделал ${count} кликов`);
+    }, 3000);
+  }
 }
