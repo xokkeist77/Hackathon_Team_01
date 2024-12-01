b5d11c4ed9666812e1cea0fd307356477ee44c4b
ururu42 <utepusechka@gmail.com>
2024-12-01 09:54:01 +0100

    Рефакторинг кода модуля фигур

--- src/modules/shape.module.js
+++ src/modules/shape.module.js
@@ -1,10 +1,12 @@
 import { Module } from '../core/module'
+import { random } from '../utils';
 
 const shapes = {
     circle: 0,
     rectangle: 1,
     polygon: 2,
 }
+
 export class ShapeModule extends Module {
     constructor(type, text, emoji) {
         super(type, text, emoji);
@@ -17,86 +19,64 @@ export class ShapeModule extends Module {
         document.querySelector('body').append(this.canvas);        
     }
 
-    trigger() {
-        this.display();
+    clearRect() {
+        this.ctx.fillStyle = `rgb(${Math.floor(random(0, 256))}, ${Math.floor(random(0, 256))}, ${Math.floor(random(0, 256))})`;
+        // this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+        this.ctx.beginPath();   
     }
 
-    display() {
-        const props = {
-            canvas: this.canvas,
-            ctx: this.ctx,
+    getRandomParameters() {
+        return {
+            x: random(0, this.canvas.width),
+            y: random(0, this.canvas.height),
+            width: random(25, 200), 
+            height: random(25, 200)
+        }
+    }
+
+    drawCircle() {
+        const {x, y, width, height} = this.getRandomParameters();
+
+        this.ctx.arc(x, y, Math.min(width, height) / 2, 0, 2 * Math.PI); 
+        this.ctx.fill();  
+    }
+
+    drawRectangle() {
+        const {x, y, width, height} = this.getRandomParameters();
+
+        this.ctx.rect(x, y, width, height); 
+        this.ctx.fill();
+    }
+
+    drawPolygon() {
+        const {x, y, width, height} = this.getRandomParameters();
+        const sides = Math.floor(random(3, 8)); 
+        const angle = (Math.PI * 2) / sides;
+
+        this.ctx.moveTo(x + width * Math.cos(0), y + height * Math.sin(0));
+
+        for (let i = 1; i < sides; i++) {
+            this.ctx.lineTo(x + width * Math.cos(angle * i), y + height * Math.sin(angle * i));
         }
 
-        const shapeType = Math.floor(Math.random()*3);
+        this.ctx.closePath();
+        this.ctx.fill();
+    }
 
-        let figure;
+    trigger() {
+        this.clearRect();
+        const shapeType = Math.floor(Math.random()*3);
         
         switch(shapeType) {
             case shapes.circle:
-                figure = new Circle(props);
+                this.drawCircle()
                 break;
             case shapes.rectangle:
-                figure = new Rectangle(props);
+                this.drawRectangle()
                 break;
             case shapes.polygon:
-                figure = new Polygon(props);
+                this.drawPolygon()
                 break;
-            default: 
-                return;
         }        
-        figure.render();
-    }    
-}
-
-//общий класс для фигур
-export class Figure {
-    constructor (props) {
-        this.props = props;
-        this.x = this.randomInRange(0, props.canvas.width);
-        this.y = this.randomInRange(0, props.canvas.height);
-        this.width = this.randomInRange(25, 200); 
-        this.height = this.randomInRange(25, 200);
-        const color = `rgb(${Math.floor(this.randomInRange(0, 256))}, ${Math.floor(this.randomInRange(0, 256))}, ${Math.floor(this.randomInRange(0, 256))})`;
-        this.props.ctx.fillStyle = color;
-    }
-
-    randomInRange(min, max) {
-        return Math.random() * (max - min) + min;
-    }
-    //очистка canvas и очистка списка подпутей перед отрисовкой новой фигуры
-    render() {
-        this.props.ctx.clearRect(0, 0, this.props.canvas.width, this.props.canvas.height);
-        this.props.ctx.beginPath();        
-    }
-}
-
-//класс кругов
-export class Circle extends Figure {
-    render() {
-        super.render();
-        this.props.ctx.arc(this.x, this.y, Math.min(this.width, this.height) / 2, 0, 2 * Math.PI); 
-        this.props.ctx.fill();      
-    }
-}
-//класс прямоугольников
-export class Rectangle extends Figure {
-    render() {
-        super.render();
-        this.props.ctx.rect(this.x, this.y, this.width, this.height); 
-        this.props.ctx.fill();
-    }   
-}
-//класс многоугольников
-export class Polygon extends Figure {
-    render() {
-        super.render();
-        const sides = Math.floor(this.randomInRange(3, 8)); 
-        const angle = (Math.PI * 2) / sides;
-        this.props.ctx.moveTo(this.x + this.width * Math.cos(0), this.y + this.height * Math.sin(0));
-        for (let i = 1; i < sides; i++) {
-            this.props.ctx.lineTo(this.x + this.width * Math.cos(angle * i), this.y + this.height * Math.sin(angle * i));
-        }
-        this.props.ctx.closePath();
-        this.props.ctx.fill();
     }
 }
